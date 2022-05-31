6bf48cf34526f3f06bfa3ac99f0f19cb9ff61f06
NvR-CDA <nvrforget1997@gmail.com>
2022-05-31 18:45:02 +0700

    Complete UI implementation

--- Assets/Scripts/Stats/GlobalHealth.cs
+++ Assets/Scripts/Stats/GlobalHealth.cs
@@ -10,6 +10,11 @@ public class GlobalHealth : MonoBehaviour
     public GameObject healthDisplay;
     public int internalHealth;
 
+    public GameObject HP100;
+    public GameObject HP75;
+    public GameObject HP50;
+    public GameObject HP25;
+
     void Start()
     {
         healthValue = 100;
@@ -24,5 +29,37 @@ public class GlobalHealth : MonoBehaviour
 
         internalHealth = healthValue;
         healthDisplay.GetComponent<Text>().text = "" + healthValue;
+
+        if (healthValue > 75)
+        {
+            HP100.SetActive(true);
+            HP75.SetActive(false);
+            HP50.SetActive(false);
+            HP25.SetActive(false);
+        }
+
+        if (healthValue <= 75 && healthValue > 50)
+        {
+            HP100.SetActive(false);
+            HP75.SetActive(true);
+            HP50.SetActive(false);
+            HP25.SetActive(false);
+        }
+
+        if (healthValue <= 50 && healthValue > 25)
+        {
+            HP100.SetActive(true);
+            HP75.SetActive(false);
+            HP50.SetActive(true);
+            HP25.SetActive(false);
+        }
+
+        if (healthValue <= 25)
+        {
+            HP100.SetActive(false);
+            HP75.SetActive(false);
+            HP50.SetActive(false);
+            HP25.SetActive(true);
+        }
     }
 }
